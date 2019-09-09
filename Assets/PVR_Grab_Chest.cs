63a95319e5a6f393db8079f7008c266c954fd213
mariohaberle <mario.haberle@gmail.com>
2019-09-09 19:46:12 +0200

    Added lever. Finished crate.

--- Assets/PVR_Grab_Chest.cs
+++ Assets/PVR_Grab_Chest.cs
@@ -7,21 +7,25 @@ namespace MarioHaberle.PlayVRoom.VR.Interaction
     public class PVR_Grab_Chest : PVR_Interactable
     {
         [Header("-----------------------------------")]
-        //[SerializeField] private Transform _handHelper;
-        [SerializeField] private Vector3 angularVelocityMock = new Vector3(1, 0, 0);
-        [SerializeField] private float multiplier = 10;
+        [SerializeField] private float _angularVelocityStrength = 10;
 
-        public Vector3 _cross;
+        private Vector3 _angularVelocityDirection;
+        private Vector3 _cross;
+
         private void FixedUpdate()
         {
-            angularVelocityMock = transform.right;
+            //Get right vector, the one where we apply angular velocity
+            _angularVelocityDirection = transform.right;
+
             if(Picked && Hand)
             {
+                //calculate cross vector so we know if hand is below or above the crate Z axis
                 _cross = Vector3.Cross(transform.forward, transform.position - Hand.Rigidbody.position);
+                //turn that into local coordinate space
                 _cross = transform.InverseTransformDirection(_cross);
-                Rigidbody.angularVelocity = angularVelocityMock * _cross.x * -multiplier;
-            }
-            
+                //apply angular velocity in the desired direction * strength
+                Rigidbody.angularVelocity = _angularVelocityDirection * _cross.x * -_angularVelocityStrength;
+            }            
         }
     } 
 }
---
+++ Assets/PVR_Lever_Pull.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MarioHaberle.PlayVRoom.VR.Interaction
+{
+    public class PVR_Lever_Pull : PVR_Interactable
+    {
+        [Header("Settings")]
+        [Header("-----------------------")]        
+        [SerializeField] private float _angularVelocityStrength = 10;
+        [SerializeField] private float _minAngle = -30;
+        [SerializeField] private float _maxAngle = 30;
+
+        //calculation infos
+        private Vector3 _crossFromInitialDirection;
+        private float _rotationAmount;
+        private Vector3 _cross;
+        private Vector3 _angularVelocityDirection;
+
+        //initial values
+        private Quaternion _initialRotation;
+        private Vector3 _initialForwardDirection;
+        private float _initialPivotRotat;
+        private Vector3 _initialPivotPosition;
+        private Quaternion _maxAngleFinalRotation;
+
+        public override void Awake()
+        {
+            base.Awake();
+
+            Rigidbody.centerOfMass = Vector3.zero;
+
+            _initialRotation = Rigidbody.rotation;
+            _initialForwardDirection = Rigidbody.transform.forward;
+            _initialPivotPosition = Rigidbody.position;
+            _initialPivotRotat = Rigidbody.transform.localRotation.eulerAngles.x;
+        }
+
+        private void FixedUpdate()
+        {
+            Rigidbody.position = _initialPivotPosition;
+            _angularVelocityDirection = Rigidbody.transform.right;
+
+            if (Picked && Hand)
+            {
+                //Calculating angle from center
+                _crossFromInitialDirection = Vector3.Cross(_initialForwardDirection, Rigidbody.position - Hand.Rigidbody.position);
+                _crossFromInitialDirection = transform.InverseTransformDirection(_crossFromInitialDirection);
+                //calculate orientation since default position
+                _rotationAmount = Rigidbody.transform.localRotation.eulerAngles.x - _initialPivotRotat;
+                if (_crossFromInitialDirection.x < 0)
+                {
+                    _rotationAmount *= -1;
+                }
+                //Calculate amount to move and in which direction
+                _cross = Vector3.Cross(Rigidbody.transform.forward, Rigidbody.position - Hand.Rigidbody.position);
+                _cross = transform.InverseTransformDirection(_cross);
+                //apply angular velocity in desired direction * strength
+                Rigidbody.angularVelocity = _angularVelocityDirection * _cross.x * _angularVelocityStrength;
+            }
+
+            //limiting rotation angle
+            if(_rotationAmount >= _maxAngle && _cross.x > 0)
+            {
+                _maxAngleFinalRotation = _initialRotation * Quaternion.Euler(new Vector3(-_maxAngle, 0, 0));
+                Rigidbody.angularVelocity = Vector3.zero;
+                Rigidbody.rotation = _maxAngleFinalRotation;
+            }
+            else if(_rotationAmount <= _minAngle && _cross.x < 0)
+            {
+                _maxAngleFinalRotation = _initialRotation * Quaternion.Euler(new Vector3(-_minAngle, 0, 0));
+                Rigidbody.angularVelocity = Vector3.zero;
+                Rigidbody.rotation = _maxAngleFinalRotation;
+            }
+        }
+    } 
+}
