90630c6be48507e1c4fe87d783c7a09119296117
YoyoMario <mario.haberle@gmail.com>
2020-03-03 23:21:52 +0100

    Added headers.

--- Assets/Scripts/Misc/PVR_Dummy.cs
+++ Assets/Scripts/Misc/PVR_Dummy.cs
@@ -6,8 +6,10 @@ namespace DivIt.PlayVRoom.Misc
 {
     public class PVR_Dummy : MonoBehaviour
     {
+        [Header("References")]
         [SerializeField] private Rigidbody _rigidbodyDummy = null;
         [SerializeField] private Transform _transformBase = null;
+        [Header("Settings")]
         [SerializeField] private float _baseStrength = 500;
         [SerializeField] private Vector3 _comPosition = Vector3.one;
 
