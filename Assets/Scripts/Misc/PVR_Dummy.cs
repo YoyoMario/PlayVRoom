18dff1eb50f9f5bcb3852f7e19fcda9ddda3aa9d
YoyoMario <mario.haberle@gmail.com>
2020-03-03 19:35:57 +0100

    Increased hand trigger collider from 0.05 -> 0.1 Whole new scene redone, recentered. pvr_dummy mass returned from 45kg - 1kg, updated angulardrag from 0.05 -> 1.

---
+++ Assets/Scripts/Misc/PVR_Dummy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DivIt.PlayVRoom.Misc
+{
+    public class PVR_Dummy : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rigidbodyDummy = null;
+        [SerializeField] private Transform _transformBase = null;
+        [SerializeField] private float _baseStrength = 500;
+        [SerializeField] private Vector3 _comPosition = Vector3.one;
+
+        private void Awake()
+        {
+            _rigidbodyDummy.centerOfMass = _comPosition;
+        }
+
+        private void FixedUpdate()
+        {
+            //Position update
+            Vector3 direction = _transformBase.position - _rigidbodyDummy.position;
+            _rigidbodyDummy.velocity = (direction * _baseStrength * Time.fixedDeltaTime);
+        }
+    } 
+}
