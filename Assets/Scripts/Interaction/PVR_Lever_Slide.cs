22ab4a79a6bdb219fcf3ea3106f5813fc54dcf80
YoyoMario <mario.haberle@gmail.com>
2020-03-04 14:04:03 +0100

    Fixed lever pull bugs and enabled realtime update of the object.

--- Assets/Scripts/Interaction/PVR_Lever_Slide.cs
+++ Assets/Scripts/Interaction/PVR_Lever_Slide.cs
@@ -9,6 +9,7 @@ namespace DivIt.PlayVRoom.VR.Interaction
     public class PVR_Lever_Slide : PVR_Interactable
     {
         [Header("-----------------------------")]
+        [SerializeField] private Transform _transformParent = null;
         [SerializeField] private float _maxMovement = 0.03f;
         [SerializeField] private float _minMovement = -0.03f;
         [Space(5)]
@@ -32,20 +33,21 @@ namespace DivIt.PlayVRoom.VR.Interaction
             Rigidbody.isKinematic = false;
             Rigidbody.constraints = RigidbodyConstraints.None;
 
-            _initialRotation = Rotation;
+            _initialRotation = Transform.localRotation;
             _lastHandledPosition = Position;
         }
 
         public override void FixedUpdate()
         {
-            _velocityDirection = Transform.up;
-            _deltaMovement = Transform.localPosition.z * -1; //-1 just to get right orientation with the cross product
-            Rigidbody.rotation = _initialRotation;
+            _velocityDirection = Transform.forward;
+            _deltaMovement = Transform.localPosition.z; //-1 just to get right orientation with the cross product
+            Rigidbody.rotation = _transformParent.rotation * Quaternion.Inverse(_initialRotation); //calculate local rotation to global
 
             if (Picked && Hand)
             {
-                _cross = Vector3.Cross(Transform.forward, Position - Hand.Rigidbody.position);
+                _cross = Vector3.Cross(Transform.up, Position - Hand.Rigidbody.position);
                 _cross = Transform.InverseTransformDirection(_cross);
+                _cross *= -1;
                 Rigidbody.velocity = _velocityDirection * _cross.x * ControllerPhysics.LeverSlideVelocityStrength;
             }
             else
@@ -56,24 +58,20 @@ namespace DivIt.PlayVRoom.VR.Interaction
                 Rigidbody.position = _lastHandledPosition;
             }
 
-            //limiting movement position
+            //limiting movement position - rigidbody
             if (_deltaMovement >= _maxMovement && _cross.x > 0)
             {
                 Rigidbody.angularVelocity = Vector3.zero;
                 Rigidbody.velocity = Vector3.zero;
-
-                Transform.localPosition = new Vector3(0, 0, _minMovement);
             }
             else if (_deltaMovement <= _minMovement && _cross.x < 0)
             {
                 Rigidbody.angularVelocity = Vector3.zero;
                 Rigidbody.velocity = Vector3.zero;
-
-                Transform.localPosition = new Vector3(0, 0, _maxMovement);
             }
 
             //Haptics
-            if(Picked && Hand)
+            if (Picked && Hand)
             {
                 _totalMovement += Rigidbody.velocity.magnitude;
                 if(_totalMovement > _hapticsAtDeltaMovement)
@@ -87,6 +85,21 @@ namespace DivIt.PlayVRoom.VR.Interaction
                         );
                     _totalMovement = 0;
                 }
+            }
+        }
+
+        private void LateUpdate()
+        {
+            _deltaMovement = Transform.localPosition.z;
+            transform.localRotation = _initialRotation;
+            //limiting movement position - transform
+            if (_deltaMovement >= _maxMovement && _cross.x > 0)
+            {
+                Transform.localPosition = new Vector3(0, 0, _maxMovement);
+            }
+            else if (_deltaMovement <= _minMovement && _cross.x < 0)
+            {
+                Transform.localPosition = new Vector3(0, 0, _minMovement);
             }
         }
 
