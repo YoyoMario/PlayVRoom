b58893c936b62c2e6d2c3aa2b53605af9edb8b5d
YoyoMario <mario.haberle@gmail.com>
2020-03-04 15:24:53 +0100

    Lever pull fixing.

--- Assets/Scripts/Interaction/PVR_Lever_Pull.cs
+++ Assets/Scripts/Interaction/PVR_Lever_Pull.cs
@@ -9,7 +9,8 @@ namespace DivIt.PlayVRoom.VR.Interaction
     public class PVR_Lever_Pull : PVR_Interactable
     {
         [Header("Settings")]
-        [Header("-----------------------")]        
+        [Header("-----------------------")]
+        [SerializeField] private Transform _transformParent = null;
         [SerializeField] private float _minAngle = -30;
         [SerializeField] private float _maxAngle = 30;
         [Space(5)]
@@ -41,21 +42,23 @@ namespace DivIt.PlayVRoom.VR.Interaction
             Rigidbody.isKinematic = false;
             Rigidbody.constraints = RigidbodyConstraints.None;
 
-            _initialRotation = Rotation;
-            _lastHandledRotation = Rotation;
+            _initialRotation = Transform.localRotation;
+            _lastHandledRotation = Transform.localRotation;
             _initialForwardDirection = Transform.forward;
-            _initialPivotPosition = Position;
+            _initialPivotPosition = Transform.localPosition;
             _initialPivotRotat = Transform.localRotation.eulerAngles.x;
         }
 
+        public Transform _dev;
         public override void FixedUpdate()
         {
+            if (!_dev) return;
             _angularVelocityDirection = Transform.right;
 
-            if (Picked && Hand)
+            if (_dev)
             {
                 //Calculating angle from center
-                _crossFromInitialDirection = Vector3.Cross(_initialForwardDirection, Position - Hand.Rigidbody.position);
+                _crossFromInitialDirection = Vector3.Cross(_initialForwardDirection, Position - /*Hand.Rigidbody.position*/ _dev.position);
                 _crossFromInitialDirection = Transform.InverseTransformDirection(_crossFromInitialDirection);
                 //calculate orientation since default position
                 _rotationAmount = Transform.localRotation.eulerAngles.x - _initialPivotRotat;
@@ -64,51 +67,52 @@ namespace DivIt.PlayVRoom.VR.Interaction
                     _rotationAmount *= -1;
                 }
                 //Calculate amount to move and in which direction
-                _cross = Vector3.Cross(Transform.forward, Position - Hand.Rigidbody.position);
+                _cross = Vector3.Cross(Transform.forward, Position - /*Hand.Rigidbody.position*/ _dev.position);
                 _cross = Transform.InverseTransformDirection(_cross);
                 //apply angular velocity in desired direction * strength
                 Rigidbody.angularVelocity = _angularVelocityDirection * _cross.x * -ControllerPhysics.LeverPullAngularVelocityStrength;
             }
-            else
-            {
-                //handling situation if user takes another collider and smacks it on our lever
-                Rigidbody.angularVelocity = Vector3.zero;
-                Rigidbody.rotation = _lastHandledRotation;
-            }
+            //else
+            //{
+            //    //handling situation if user takes another collider and smacks it on our lever
+            //    Rigidbody.angularVelocity = Vector3.zero;
+            //    Rigidbody.rotation = _lastHandledRotation;
+            //}
+
             //to fix lever movement
             Rigidbody.velocity = Vector3.zero;
-            Rigidbody.position = _initialPivotPosition;
+            Rigidbody.position = _transformParent.TransformPoint(_initialPivotPosition);
 
             //limiting rotation angle
             if (_rotationAmount >= _maxAngle && _cross.x > 0)
             {
-                _maxAngleFinalRotation = _initialRotation * Quaternion.Euler(new Vector3(-_maxAngle, 0, 0));
+                _maxAngleFinalRotation = _transformParent.rotation * (_initialRotation) * Quaternion.Euler(new Vector3(-_maxAngle, 0, 0));
                 Rigidbody.angularVelocity = Vector3.zero;
                 Rigidbody.rotation = _maxAngleFinalRotation;
             }
-            else if(_rotationAmount <= _minAngle && _cross.x < 0)
+            else if (_rotationAmount <= _minAngle && _cross.x < 0)
             {
-                _maxAngleFinalRotation = _initialRotation * Quaternion.Euler(new Vector3(-_minAngle, 0, 0));
+                _maxAngleFinalRotation = _transformParent.rotation * (_initialRotation) *  Quaternion.Euler(new Vector3(-_minAngle, 0, 0));
                 Rigidbody.angularVelocity = Vector3.zero;
                 Rigidbody.rotation = _maxAngleFinalRotation;
             }
 
-            //Haptics
-            if(Picked && Hand)
-            {
-                _totalRotationAngle += Rigidbody.angularVelocity.magnitude;
-                if (_totalRotationAngle > _hapticsAtAngle)
-                {
-                    HapticFeedbackManager.HapticeFeedback(
-                        _movementHaptics.SecondsFromNow,
-                        _movementHaptics.Duration,
-                        _movementHaptics.Frequency,
-                        _movementHaptics.Amplitude,
-                        Hand.InputSource
-                        );
-                    _totalRotationAngle = 0;
-                }
-            }
+            ////Haptics
+            //if(Picked && Hand)
+            //{
+            //    _totalRotationAngle += Rigidbody.angularVelocity.magnitude;
+            //    if (_totalRotationAngle > _hapticsAtAngle)
+            //    {
+            //        HapticFeedbackManager.HapticeFeedback(
+            //            _movementHaptics.SecondsFromNow,
+            //            _movementHaptics.Duration,
+            //            _movementHaptics.Frequency,
+            //            _movementHaptics.Amplitude,
+            //            Hand.InputSource
+            //            );
+            //        _totalRotationAngle = 0;
+            //    }
+            //}
         }
 
         public override void OnCollisionStay(Collision collision)
@@ -121,7 +125,7 @@ namespace DivIt.PlayVRoom.VR.Interaction
             base.OnDrop();
 
             //recording the lever rotation at the moment when user leaves interacting
-            _lastHandledRotation = Rotation;
+            _lastHandledRotation = Transform.localRotation;
         }
     } 
 }
